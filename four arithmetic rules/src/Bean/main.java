96c56c94b4c8a89efcb0f2198efb0b6729a95f87
CIELIEL <1615514806@qq.com>
2020-10-11 11:37:22 +0800

    fourCal

---
+++ four arithmetic rules/src/Bean/main.java
@@ -0,0 +1,26 @@
+package Bean;
+
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args){
+        //BinaryTree binaryTree=new BinaryTree(2);
+        //binaryTree.createBTree(10,1);
+
+        CreatSubjects creatSubjects=new CreatSubjects();
+        creatSubjects.creatSubject(10,1);
+
+//System.out.println("-2".indexOf(0));
+        //BinaryTree binaryTree= (BinaryTree) BinaryList.saveList.get(0);
+        //System.out.println(binaryTree.toString().indexOf()+"------");
+        //System.out.println(BinaryList.saveList.get(0)+"-------");
+        //System.out.println(BinaryList.saveList.get(0).getRchild().getStr()+"-------");
+       // System.out.println(BinaryList.saveList.get(0).getLchild()+"-------");
+        //System.out.println(RootSave.rootList.get(0));
+        //int c=Integer.parseInt("1/3");
+        //System.out.println(c);
+       // DuplicateCheck duplicateCheck=new DuplicateCheck();
+       // boolean b=duplicateCheck.CompareStringByChar("423","234");
+        //System.out.println(b);
+    }
+    }
